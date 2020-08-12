9e8dacae97a496c14e53595df70e2f9b253086f8
Mehtab Alam <mehtaba728@mail.com>
2020-08-12 12:55:22 +0500

    giving some top margin to the container

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   componentDidMount() {
-    this.onTermSubmit('buildings');
+    this.onTermSubmit("buildings");
   }
 
-  onTermSubmit = async term => {
-    const response = await youtube.get('/search', {
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
-        q: term
-      }
+        q: term,
+      },
     });
 
     this.setState({
       videos: response.data.items,
-      selectedVideo: response.data.items[0]
+      selectedVideo: response.data.items[0],
     });
   };
 
-  onVideoSelect = video => {
+  onVideoSelect = (video) => {
     this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
-      <div className="ui container">
+      <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
         <div className="ui grid">
           <div className="ui row">
